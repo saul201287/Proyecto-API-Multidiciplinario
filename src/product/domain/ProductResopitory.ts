f9690a645105c4c4c2b06f672a090a8f52d536b5
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-15 14:06:26 -0600

    yA

--- src/product/domain/ProductResopitory.ts
+++ src/product/domain/ProductResopitory.ts
@@ -1,8 +1,6 @@
 import { Product } from "./Product";
 
 export interface ProductRepository {
-  getAll(): Promise<Product[] | null>;
-  getById(userId: number): Promise<Product | null>;
   createProduct(
     name: string,
     description: string,
